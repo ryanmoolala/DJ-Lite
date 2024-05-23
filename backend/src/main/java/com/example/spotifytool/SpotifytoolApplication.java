2cd852936b7ca01eceaac6781c5f937ae72fb1e3
ryantjy <ryantjy2002@gmail.com>
2024-05-23 21:13:42 +0800

    backend dockerfile setup

--- backend/src/main/java/com/example/spotifytool/SpotifytoolApplication.java
+++ backend/src/main/java/com/example/spotifytool/SpotifytoolApplication.java
@@ -2,6 +2,7 @@ package com.example.spotifytool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,4 +11,9 @@ public class SpotifytoolApplication {
     public static void main(String[] args) {
 		SpringApplication.run(SpotifytoolApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
 }
