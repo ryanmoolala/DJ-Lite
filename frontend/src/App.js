520c44585216630038e250a50c4aa7b56020e316
ryantjy <ryantjy2002@gmail.com>
2024-05-23 18:13:34 +0800

    front end

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import LoginPage from './Components/LoginPage';
+import HomePage from './Components/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
