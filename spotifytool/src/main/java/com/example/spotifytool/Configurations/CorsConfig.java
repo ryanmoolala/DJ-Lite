29defefc9125db8d4b9f8eea41dc5bf777b12692
ryantjy <ryantjy2002@gmail.com>
2024-05-26 23:14:44 +0800

    global cors enabled

--- spotifytool/src/main/java/com/example/spotifytool/Configurations/CorsConfig.java
+++ spotifytool/src/main/java/com/example/spotifytool/Configurations/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-    registry.addMapping("/**")
+    registry.addMapping("/api/**")
       .allowedOrigins("http://localhost:3000")
       .allowedMethods("GET", "POST")
       .maxAge(3600);
