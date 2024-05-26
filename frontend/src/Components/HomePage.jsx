eda263b3c8ffebca43158933ae1e3b6699c5cce8
ryantjy <ryantjy2002@gmail.com>
2024-05-26 23:14:04 +0800

    api test

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -2,21 +2,33 @@ import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
 
-    const [userTopArtist, setUserTopArtist] = useState();
-    
-    useEffect(() => {
-        fetch("http://localhost:8080/api/user-top-artist")
-        .then(response => response.json)
-        .then(data => {
-            console.log(data)
-            setUserTopArtist(data)
-        })
-    })
 
+    const handleApiRequest = () => {
+        try {
+            const response =  fetch("/api/user-top-artist", {
+                method: "GET", // or "POST" if needed
+                // Add any necessary headers here (e.g., authorization token)
+            });
+    
+            if (response.ok) {
+                // Handle the successful response (e.g., update state, display data)
+                const data = response.json();
+                console.log("API response:", data);
+            } else {
+                console.error("API request failed:", response.status, response.statusText);
+            }
+
+
+        } catch (error) {
+            console.error("Error sending API request:", error);
+        }
+    };
+
+    
     return (
         <div>
             <h1>Spotify Home Page</h1>
-            {userTopArtist ? <p>{userTopArtist}</p> : <h1>Loading...</h1>}
+            <button onClick={handleApiRequest}>Send API Request</button>
         </div>
     )
 }
