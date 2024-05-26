33c27101ec03a9e5ba8eb0d011cad279460d5450
ryantjy <ryantjy2002@gmail.com>
2024-05-26 23:15:02 +0800

    added tokens to session

--- spotifytool/src/main/java/com/example/spotifytool/AuthorizationFlowPKCE/AuthorizationCodeController.java
+++ spotifytool/src/main/java/com/example/spotifytool/AuthorizationFlowPKCE/AuthorizationCodeController.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.UUID;
 
-
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
@@ -17,14 +17,12 @@ import se.michaelthelin.spotify.requests.authorization.authorization_code.Author
 import se.michaelthelin.spotify.requests.authorization.authorization_code.pkce.AuthorizationCodePKCERequest;
 
 import org.apache.hc.core5.http.ParseException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api")
 public class AuthorizationCodeController {   
 
@@ -39,11 +37,11 @@ public class AuthorizationCodeController {
             .setClientSecret(SpotifyDetails.clientSecret)
             .setRedirectUri(redirectUri)
             .build();
-    
+
 
     /*RETURNS THE LOGIN PAGE URL */
     @GetMapping("/login")  
-    public String getUri() throws ParseException {
+    public static String getUri() throws ParseException {
         AuthorizationCodeUriRequest authCodeUriReq = spotifyApi
         .authorizationCodePKCEUri(codeChallenge)
         .state(securityState)
@@ -54,37 +52,32 @@ public class AuthorizationCodeController {
     }
 
 
-    public AuthorizationCodePKCERequest getUri2(String code, String verifier) {
+    public static AuthorizationCodePKCERequest getUri2(String code, String verifier) {
         return spotifyApi.authorizationCodePKCE(code, verifier).build();
     }
 
-
-
     /*RETURNS THE STRING AUTHENTHICATION CODE */
     @GetMapping("/callback")
-    public String getAuthCode(@RequestParam("code") String authCode, HttpServletResponse response) throws IOException {
-        AuthorizationCodeController inst = new AuthorizationCodeController();
+    public String getAuthCode(@RequestParam("code") String authCode, HttpServletResponse response, HttpSession session) throws IOException {
         String code = authCode;
-        AuthorizationCodePKCERequest authorizationCodePKCERequest = inst.getUri2(code, codeVerifier);
-
+        AuthorizationCodePKCERequest authorizationCodePKCERequest = spotifyApi.authorizationCodePKCE(code, codeVerifier).build();
+        
         try {
             final AuthorizationCodeCredentials credentials = authorizationCodePKCERequest.execute();
-
-            System.out.println("Token type : " + credentials.getTokenType());
-            System.out.println("access : " + credentials.getAccessToken());
-            System.out.println("refresh : " + credentials.getRefreshToken());
-            System.out.println("Expires in : " + credentials.getExpiresIn());            
 
             spotifyApi.setAccessToken(credentials.getAccessToken());
             spotifyApi.setRefreshToken(credentials.getRefreshToken());
 
+            session.setAttribute("type", credentials.getTokenType());
+            session.setAttribute("access", credentials.getAccessToken());
+            session.setAttribute("refresh", credentials.getRefreshToken());
+            session.setMaxInactiveInterval(credentials.getExpiresIn());
+
+
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             System.out.println("Error: " + e.getMessage());
         }
-
-        response.sendRedirect("http://localhost:3000/home");
-
+        response.sendRedirect("http://localhost:3000/api/home");
         return spotifyApi.getAccessToken();
     }
-    
 }
