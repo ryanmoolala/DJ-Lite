73e4129532044d0878a022afdbd794d13f6ae53f
ryantjy <ryantjy2002@gmail.com>
2024-05-27 02:18:28 +0800

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import LoginPage from './Components/LoginPage';
 import HomePage from './Components/HomePage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LoginPage/>} />
         <Route path="/api/home" element={<HomePage/>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
