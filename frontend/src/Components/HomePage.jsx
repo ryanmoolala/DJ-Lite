c30e5be7a6871ea7bfa028ca291764378e246e31
ryantjy <ryantjy2002@gmail.com>
2024-06-05 21:02:18 +0800

    frontend

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   const userTopArtist = () =>
     fetch("/api/user-top-artist?term=0&limit=10", { method: "GET" })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data)
+        navigate('/api/dashboard')
+      });
 
   const userTopSong = () =>
     fetch("/api/user-top-song?term=0&limit=10", { method: "GET" })
@@ -14,11 +20,6 @@ const HomePage = () => {
   const userTopGenres = () => {
     console.log("top genres");
   };
-
-  const userStats = () =>
-    fetch("/api/user-profile", { method: "GET" })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
 
   const findArtist = () =>
     fetch("/api/findArtist", { method: "GET" })
@@ -35,6 +36,12 @@ const HomePage = () => {
       .then((response) => response.text())
       .then((link) => window.open(link));
 
+  const findSpotifyPlaying = () =>
+    fetch("/player/findSpotifyPlaying", { method: "GET" })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+
   return (
     <div>
       <h1>Spotify Home Page</h1>
@@ -48,13 +55,15 @@ const HomePage = () => {
         Send API Request for user-top-genres
       </button>
       <div></div>
-      <button onClick={userStats}>Send API Request for user-stats</button>
-      <div></div>
       <button onClick={findArtist}>Send API Request for artist</button>
       <div></div>
       <button onClick={findTrack}>Send API Request for track</button>
       <div></div>
       <button onClick={findAlbum}>Send API Request for album</button>
+      <div></div>
+      <button onClick={findSpotifyPlaying}>
+        Send API Request for current song
+      </button>
     </div>
   );
 };
