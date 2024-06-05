b64b7832774171b7ea9e14fe4c48aa2eac788fd0
ryantjy <ryantjy2002@gmail.com>
2024-06-05 21:03:52 +0800

    Pause Play button

---
+++ frontend/src/Components/Music Slider/Button.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { IoIosPlay } from "react-icons/io";
+import { IoPauseSharp } from "react-icons/io5";
+
+const Button = ({ isPlaying, setIsPlaying }) => {
+  const [deviceID, setDeviceID] = useState('');
+
+  useEffect(() => {
+    fetch("/player/device", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => setDeviceID(data.devices[0].id))
+      .catch((error) => console.log(error));
+  });
+
+  const pause = () => {
+    setIsPlaying(!isPlaying);
+    fetch(`/player/pause/${deviceID}`, { method: "PUT" }).catch((error) =>
+      console.log(error)
+    );
+  };
+
+  const resume = () => {
+    setIsPlaying(!isPlaying);
+    fetch(`/player/resume/${deviceID}`, { method: "PUT" }).catch((error) =>
+      console.log(error)
+    );
+  };
+
+  return (
+    <div>
+      <button onClick={() => (isPlaying ? pause() : resume())}>
+        <div
+          class={`${
+            isPlaying ? "block" : "hidden"
+          } transition-transform duration-300 transform hover:scale-110`}
+        >
+          <IoPauseSharp />
+        </div>
+        <div
+          class={`${
+            isPlaying ? "hidden" : "block"
+          } transition-transform duration-300 transform hover:scale-110`}
+        >
+          <IoIosPlay />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
