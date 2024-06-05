ce29405146c41e3aa2cba840e5a705bb92e5a3bf
ryantjy <ryantjy2002@gmail.com>
2024-06-05 21:01:02 +0800

    added animation settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 5s none',
+
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+        'loop-scroll-reverse' : 'loop-scroll-reverse 50s linear infinite',
+      },
+
+      keyframes: {
+
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+
+        'loop-scroll-reverse': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(100%)' },
+        },
+
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        },
+
+      },
+
       textShadow: {
         outline: '2px 2px 0px #000', // Customize the shadow here
       },
