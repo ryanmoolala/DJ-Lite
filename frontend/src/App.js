16177a072b29e84f02733577a1c3c7f6525e6496
ryantjy <ryantjy2002@gmail.com>
2024-06-05 21:01:35 +0800

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import LoginPage from './Components/LoginPage';
 import HomePage from './Components/HomePage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
+        <Route path="/" element={<LoginPage/>}/>
         <Route path="/api/home" element={<HomePage/>} />
+        <Route path="/api/dashboard" element={<Dashboard/>}/>
       </Routes>
     </Router>
   );
