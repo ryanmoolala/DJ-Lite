60e5412bdf01c6664d3787b374ec563d10e6ef49
ryantjy <ryantjy2002@gmail.com>
2024-06-05 21:02:08 +0800

    The Main apge

---
+++ frontend/src/Components/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Top from "./Top";
+import Body from "./Body";
+
+const Dashboard = () => {
+    
+    return (
+        <div class="flex-col space-y-28">
+            <Top/>
+            <Body/>
+        </div>
+    )
+
+}
+
+export default Dashboard;
