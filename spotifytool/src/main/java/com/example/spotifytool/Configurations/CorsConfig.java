6e3b05b3ebb9a62333926ce2abcbf668800a6bb2
ryantjy <ryantjy2002@gmail.com>
2024-06-08 17:37:20 +0800

    .

--- spotifytool/src/main/java/com/example/spotifytool/Configurations/CorsConfig.java
+++ spotifytool/src/main/java/com/example/spotifytool/Configurations/CorsConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
     registry.addMapping("/api/**")
       .allowedOrigins("http://localhost:3000")
       .allowedMethods("GET", "POST")
